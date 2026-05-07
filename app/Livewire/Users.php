a06e80ff60d54cde3438fda8a72142d83c3345e0
Fauzan <fauzanzha09@gmail.com>
2026-05-07 21:09:16 +0700

    Feat: displaying list user on database (realtime added livewire)

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,11 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
     // public $title = 'Users Page';
+    public function createUser()
+    {
+        User::create([
+            'name' => 'User Baru',
+            'email' => 'email2@email.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
 
     public function render()
     {
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,4 +1,16 @@
-<div>
+<div class="w-1/2 m-auto my-10">
     <h1 class="text-3xl font-semibold">{{ $title }}</h1>
     <p>Users Count:{{ count($users) }}</p>
+    <button wire:click="createUser"
+        class="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 my-2 cursor-pointer">Add
+        user</button>
+
+    <hr class="border-1 my-5">
+
+    <h2 class="text-2xl font-semibold mb-2">Users List</h2>
+    <ul class="list-disc">
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>
